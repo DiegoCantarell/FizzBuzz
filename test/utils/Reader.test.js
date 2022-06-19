24cc0b95c2db1ad0a1b00571430f07d28d33b42d
kantrax <diegogccantarell@gmail.com>
2022-06-18 22:33:09 -0500

    Step 21: Adding code to Reader Test, it must not to be Undefined

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,26 @@
+const Reader = require('./../../lib/utils/Reader')
+    describe("Unit Test for Reader Class", () => {
+	
+	test('Create a Reader -> readJsonFile(filePath)', () =>{
+		//Here goes the code to use in the app
+	    
+        /*const path = {
+            "name": "Woopa1",
+            "githubUsername": "ajolonauta1",
+            "score": 1,
+            "mission": "node",
+            "stacks": [
+            "javascript",
+            "reasonML",
+            "elm"
+            ]
+            }
+        */
+
+        const path = "explorers.json"
+        const result = Reader.readJsonFile(path)
+
+		//expect(user.bio).toBe("Bio")
+		expect(result).not.toBeUndefined()
+	});
+})
