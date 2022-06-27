cdabe2db83d6bf6df99d7c1a5b191c2de019846c
DiegoCantarell <diegogccantarell@gmail.com>
2022-06-26 21:30:39 -0500

    Step 43:  Adding test for ExplorerController class and the test for the static method getExplorersUsernamesByMission

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -6,6 +6,15 @@ describe('Test for ExplorerController',() => {
         expect(ExplorerController.getExplorersByMission(mission)).toBeDefined();
         expect(ExplorerController.getExplorersByMission(mission)).not.toBeUndefined()
         expect(ExplorerController.getExplorersByMission(mission).length).toBe(10)
-
     })
+
+
+    test('#2 Test for Static Method getExplorersUsernamesByMission',() => {
+        mission = "node"
+        expect(ExplorerController.getExplorersUsernamesByMission(mission)).toBeDefined();
+        expect(ExplorerController.getExplorersUsernamesByMission(mission)).not.toBeUndefined()
+        expect(ExplorerController.getExplorersUsernamesByMission(mission).length).toBe(10)
+    })
+
+
 })
